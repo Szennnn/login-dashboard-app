b8c3b3ed10b6900ac8cbcc384dbfc45298cfdd0b
Your Name <seanlee@cakexp.com>
2025-05-16 09:34:07 +0800

    Add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', loadComponent: () => import('./login/login.component').then((m) => m.LoginComponent)},
-    {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.DashboardComponent) },
+    {path: 'dashboard', loadComponent: () => import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+        canActivate: [AuthGuard] 
+     },
 ];
